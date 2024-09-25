b2c06e4bc10e5e05e7c77897c255d09cf6d7e731
Maryam Wardak <mmwk183@gmail.com>
2024-09-25 02:23:45 +0100

    Add basic functionality and typo corrections

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('todo-form');
-const task = document.getElementById('task-input');
-const list = document.getElementById('task-list');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
 
 
 form.addEventListener('submit', function(e) {
@@ -8,8 +8,24 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     const task = taskInput.value;
 
+        // make a new list item
     const li = document.createElement('li');
+    li.textContent = task;
 
+
+        // add comp btn to the task
+    const completeBtn = document.createElement('button');
+    completeBtn.textContent = '✔️';
+    completeBtn.style.marginLeft = '10px';
+    li.appendChild(completeBtn)
+
+        //adding edit btn
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    editBtn.style.marginLeft = '10px';
+    li.appendChild(editBtn);
+
+        //adding delete btn
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Delete';
     deleteBtn.style.marginLeft = '10px';
@@ -19,8 +35,23 @@ form.addEventListener('submit', function(e) {
 
     taskInput.value='';
 
+    //comp event listener
+    completeBtn.addEventListener('click', function() {
+        li.classList.toggle('completed');
+    
+    });
+
+    //edit event listener
+    editBtn.addEventListener('click', function(){
+        const newTask = prompt("Edit your task:", li.firstChild.textContent);
+        if (newTask) {
+            li.firstChild.textContent = newTask;
+        }
+    })
+
     deleteBtn.addEventListener('click', function(){
         taskList.removeChild(li);
+
 
     });
 
